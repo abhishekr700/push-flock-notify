f916325565ac2cc1c3e005f12c8fe87c451d26cd
Abhishek Ranjan <abhishekr700@gmail.com>
2020-03-19 22:32:36 +0530

    Send flock notificatioN & update workflow
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- src/notify.js
+++ src/notify.js
@@ -1,1 +1,13 @@
 console.log("Hey I'm Running");
+
+const axios = require("axios");
+
+(async ()=>{
+    let data = await axios
+        .post(process.env.WEBHOOK, {
+            "text": "New push to github detected"
+        })
+    data = data.data
+    console.log(data);
+    
+})()
